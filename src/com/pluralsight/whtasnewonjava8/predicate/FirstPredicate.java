19de0e48404c2c32c24f8e7ca0ac6e130906aa34
Joao Lopes <joao.lopes@sinapsisenergia.com>
2018-12-17 23:13:52 -0200

    map, flatmap, intermediary methods and final methods

---
+++ src/com/pluralsight/whtasnewonjava8/predicate/FirstPredicate.java
@@ -0,0 +1,24 @@
+package com.pluralsight.whtasnewonjava8.predicate;
+
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class FirstPredicate {
+
+	public static void main(String[] args) {
+		
+		Stream<String> stream = Stream.of("one", "two", "three", "four", "five");
+		
+		@SuppressWarnings("unused")
+		Predicate<String> p1 = s -> s.length() > 3;
+		Predicate<String> p2 = Predicate.isEqual("two");
+		Predicate<String> p3 = Predicate.isEqual("five");
+		
+		//encadeando predicados
+		stream.
+			filter(p2.or(p3)).
+			forEach(System.out::println);
+		
+	}
+	
+}
